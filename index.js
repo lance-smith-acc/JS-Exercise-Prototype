9b1f4d6da0c40aac9b8d85a61a1c06498f5bffc1
Lance Smith <57012297+lance-smith-acc@users.noreply.github.com>
2019-11-13 14:55:10 -0600

    Did challenges through Task 2 including stretch.

--- index.js
+++ index.js
@@ -39,8 +39,23 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name,
+  this.age = age,
+  this.stomach = []
+}
 
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length != 10) {
+    this.stomach.push(someFood);
+  }
+}
+Person.prototype.poop = function () {
+  this.stomach.splice(0);
+}
+
+Person.prototype.toString= function (){
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,9 +72,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model,
+  this.milesPerGallon = milesPerGallon,
+  this.tank = 0,
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function (gallons) {
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function (distance) {
+
+  if ((this.milesPerGallon*this.tank) < distance) {
+    this.odometer = this.odometer + (this.tank*this.milesPerGallon),
+    this.tank = 0;
+    return `I rant out of fuel at ${this.odometer} miles!`
+  }
+
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance/this.milesPerGallon);
+}
+
 
 /*
   TASK 3
